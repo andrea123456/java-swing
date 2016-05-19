f6a6ef799ee36de2949aebd4d5d1a1f1b1ced0a0
tss <tss@debian-tss24>
2016-05-19 21:30:08 +0200

    grafica

--- src/Lezione10/App.java
+++ src/Lezione10/App.java
@@ -5,14 +5,29 @@
  */
 package Lezione10;
 
+import java.awt.Container;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  *
  * @author tss
  */
 public class App {
     public static void main(String[]args) {
+        JFrame f = new JFrame("Esempio swing");
         
-       System.out.println("java-swing main running");
-       System.out.println("jciao");
+        f.setBounds(100, 100, 300, 200);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        Container contenPane = f.getContentPane();
+        JButton cmd = new JButton("invia");
+        contenPane.add(cmd);
+        contenPane.setLayout(null);
+        cmd.setBounds(50, 50, 100, 50);
+        f.setVisible(true);
+        
+        
+     
        }
 }
